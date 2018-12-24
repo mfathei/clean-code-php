1117869d61bb1fc6f6642f98a28c8c6c57a15b6f
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-12-24 22:28:48 +0200

    config Factories and DataTables

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,16 @@
 <?php
+
+use CleanPhp\Invoicer\Domain\Entity\Customer;
+use CleanPhp\Invoicer\Domain\Entity\Invoice;
+use CleanPhp\Invoicer\Domain\Entity\Order;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\CustomerTable;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\InvoiceTable;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\OrderTable;
+use CleanPhp\Invoicer\Persistence\Zend\TableGateway\TableGatewayFactory;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\AdapterServiceFactory;
+use Zend\Hydrator\ClassMethods;
+
 /**
  * Global Configuration Override
  *
@@ -12,5 +24,51 @@
  */
 
 return [
-    // ...
+    "service_manager" => [
+        "factories" => [
+            Adapter::class => AdapterServiceFactory::class,
+            "CustomerTable" => function ($sm) {
+                $factory = new TableGatewayFactory();
+                $hydrator = new ClassMethods();
+
+                return new CustomerTable(
+                    $factory->createGateway(
+                        $sm->get(Adapter::class),
+                        $hydrator,
+                        new Customer(),
+                        'customers'
+                    ),
+                    $hydrator
+                );
+            },
+            "InvoiceTable" => function ($sm) {
+                $factory = new TableGatewayFactory();
+                $hydrator = new ClassMethods();
+
+                return new InvoiceTable(
+                    $factory->createGateway(
+                        $sm->get(Adapter::class),
+                        $hydrator,
+                        new Invoice(),
+                        'invoices'
+                    ),
+                    $hydrator
+                );
+            },
+            "OrderTable" => function ($sm) {
+                $factory = new TableGatewayFactory();
+                $hydrator = new ClassMethods();
+
+                return new OrderTable(
+                    $factory->createGateway(
+                        $sm->get(Adapter::class),
+                        $hydrator,
+                        new Order(),
+                        'orders'
+                    ),
+                    $hydrator
+                );
+            },
+        ],
+    ],
 ];
